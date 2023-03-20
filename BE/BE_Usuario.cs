2f94462b5b5d214555926af8f77b4444e8580ec2
RiVenJona <jonathaneig45@gmail.com>
2023-03-20 15:10:26 -0500

    Se agrega el proyecto

---
+++ BE/BE_Usuario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BE
+{
+    public class BE_Usuario
+    {
+        public int IdUsuario{get; set;}
+        public string User { get; set;}
+        public string Password { get; set;}
+        public int State { get; set;}
+    }
+}
---
+++ DA/DA_Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using BE;
+namespace DA
+{
+    public class DA_Usuario
+    {
+        public List<BE_Usuario> ListaUsuario()
+        {
+            using (SqlConnection cn=new Sqlconnection())
+            {
+
+            }
+            return null;
+        }
+    }
+}
